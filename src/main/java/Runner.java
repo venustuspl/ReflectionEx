f9581583fd6beabe96cea9ee14a91abdca65d5c5
venustuspl <venustus.pl@gmail.com>
2020-05-19 22:12:38 +0200

    0.3

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,9 +2,7 @@ import java.lang.reflect.Constructor;
 
 public class Runner {
     public static void main(String[] args) throws Throwable{
-        byte[] tricky = {112, 108, 46, 106, 97, 118, 97, 115, 116, 97, 114, 116, 46, 84, 114, 105, 99, 107, 121};
-
-        Class trickyClass = Class.forName(new String(tricky));
+        Class trickyClass = Class.forName("Tricky");
         Constructor constructor = trickyClass.getDeclaredConstructor(String.class);
         constructor.setAccessible(true);
         Object trickyObj = constructor.newInstance("Java Start");
